89d630790d0041105bb2acf0777b8a4f978260ae
Marcossesh <marcosviniramos62@gmail.com>
2025-11-01 12:53:24 -0300

    fix: move package to top of Cursos.java

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import br.com.dio.desafio.dominio.Bootcamp;
+import br.com.dio.desafio.dominio.Mentorias;
+import br.com.dio.desafio.dominio.Cursos;
+import br.com.dio.desafio.dominio.Devs;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Sistema de Gerenciamento de Bootcamps");
+        Bootcamp bootcamp = new Bootcamp("Bootcamp Java", "Aprenda Java do zero");
+        Mentorias mentoria = new Mentorias("Mentoria Java", "Tire suas dúvidas sobre Java", "10/10/2023", "14:00");
+        Cursos curso = new Cursos("Java Básico", "Introdução ao Java", "Iniciante");
+
+
+        
+        Devs dev = new Devs("João", "joao@example.com");
+        System.out.println(bootcamp.getNome());
+        System.out.println(bootcamp.getDescricao());
+        System.out.println("Dev: " + dev.getNome() + ", Email: " + dev.getEmail());
+        System.out.println("Curso: " + curso.getTitulo() + ", Nível: " + curso.getNivel());
+        curso.inscreverDev(dev);
+        System.out.println("Devs inscritos no curso: " + curso.getDevsInscritos().size());
+        System.out.println("Mentoria: " + mentoria.getTitulo() + ", Data: " + mentoria.getDataMentoria() + ", Horário: " + mentoria.getHorarioMentoria());
+        System.out.println("XP da Mentoria: " + mentoria.calcularXp());
+    }
+}
